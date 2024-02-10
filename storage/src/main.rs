7807d30441e94c8f7bb49fa335b1043409513ff5
Philippe Assis <codephilippe@gmail.com>
2024-02-10 03:20:45 -0300

    0.4.5

--- storage/src/main.rs
+++ storage/src/main.rs
@@ -1,16 +1,40 @@
-mod writer;
-mod read;
-mod valuetable;
+use valu3::{prelude::*, vec_value};
+use valu3_parquet::Table;
 
-use writer::writer_parquet;
-use read::read_parquet;
+enum Key {
+    Name,
+    Age,
+    IsStudent,
+}
 
+impl Into<&str> for Key {
+    fn into(self) -> &'static str {
+        match self {
+            Key::Name => "name",
+            Key::Age => "age",
+            Key::IsStudent => "is_student",
+        }
+    }
+}
 
+fn main() {
+    let mut table = Table::new();
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    writer_parquet()?;
+    table.add(Key::Name.into(), vec_value!["Alice", "Bob", "Charlie"]);
+    table.add(Key::Age.into(), vec_value![25, 30, 35]);
+    table.add(Key::IsStudent.into(), vec_value![true, false, true]);
 
-    read_parquet()?;
+    table.load().unwrap();
 
-    return Ok( ());
+    table.to_parquet("example.parquet").unwrap();
+
+    let table = Table::from_parquet("example.parquet").unwrap();
+
+    let name: &Vec<Value> = table.get(Key::Name.into()).unwrap();
+    let age = table.get(Key::Age.into()).unwrap();
+    let is_student = table.get(Key::IsStudent.into()).unwrap();
+
+    assert_eq!(name, &vec_value!["Alice", "Bob", "Charlie"]);
+    assert_eq!(age, &vec_value![25, 30, 35]);
+    assert_eq!(is_student, &vec_value![true, false, true]);
 }
